78c9e1b93064e0796f029a4e4cb648afae83b61d
Anish <anishs1207@gmail.com>
2026-01-11 21:15:28 +0530

    chore: config

--- scripts/activate_backend.sh
+++ scripts/activate_backend.sh
@@ -4,3 +4,9 @@ pip install -r requirements.txt
 python -m uvicorn main:app --reload
 # run ./activate.sh here: or bash <file-name.sh>
 # python -m venv venv
+
+
+# frontend:
+cd web
+npm run dev
+npm run build
